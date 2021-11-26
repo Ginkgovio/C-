697c05d68c23dc1a98ee1b16dd2f5ad0c823e725
Aless0a <2511842759@qq.com>
2021-11-26 20:46:24 +0800

    X形图案

---
+++ C-4.2.cpp
@@ -0,0 +1,32 @@
+//X形图案
+#include <stdio.h>
+int main()
+{
+	int n = 0;
+	while (scanf("%d", &n) != EOF) 
+	{
+		for (int i = 0; i < n; i++) 
+		{
+			for (int j = 0; j < n; j++)
+			{
+				if (i == j)
+				{
+					printf("*");
+				}
+				else if(i+j==4)
+				{
+					printf("*");
+				}
+				else 
+				{
+					printf(" ");
+				}
+			}
+			printf("\n");
+		
+		}
+	
+	}
+
+	return 0;
+}
